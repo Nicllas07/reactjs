3b1984411aebe4a6413322579529da2bde290526
Nicllas07 <moraesleonardo417@gmail.com>
2024-09-02 17:51:59 +0000

    clique adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 
 import './App.css'
 
+function dizerOla(){
+  alert('Olá, como vai?')
+}
 
-let alunos = [
-  {nome: 'Savalo Horse', cursos: ['DS', 'ADM']},
-  {nome: 'Miranata Filoxina', cursos: ['MA', 'ADM']},
-];
 function App() {
   return (
    <>
-    {
-      alunos.map((element) => (
-        <div key={element.nome}>
-          {element.nome}
-          {
-           element.cursos.map((cursos) => (
-           <h1 key={curso}> {curso} </h1>
-          ))
-          }
-        </div>
-      ))
-    }
+    <button onClick={dizerOla}>Clique aqui</button>
    </>
   )
 }
